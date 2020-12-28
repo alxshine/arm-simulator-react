cdd1cc06e8b0df460f95f492cc2a9678ed348597
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-12-28 14:34:47 +0100

    Nice commit message

--- src/Registers.tsx
+++ src/Registers.tsx
@@ -1,16 +1,30 @@
-import React from "react"
-import "./Registers.css"
+import React from 'react'
+import './Registers.css'
 
-interface IProps{
-    registers: Array<number>
+interface IProps {
+  registers: Array<number>
 }
 
-export const Registers : React.FC<IProps> = ({registers}) => {
+const specialRegisterMap = new Map<number, string>()
+specialRegisterMap.set(11, 'FP')
+specialRegisterMap.set(13, 'SP')
+specialRegisterMap.set(14, 'LR')
+specialRegisterMap.set(15, 'PC')
 
-    
-    return(
-        <div id="Registers">
-            R0: {registers[0]}
-        </div>
-    )
+export const Registers: React.FC<IProps> = ({ registers }) => {
+  const rows = registers.map((value, index) => (
+    <tr>
+      <td className="registerName">
+        R{index}
+        {specialRegisterMap.has(index) ? ` (${specialRegisterMap.get(index)})` : ''}
+      </td>
+      <td className="registerValue">{value}</td>
+    </tr>
+  ))
+
+  return (
+    <div id="Registers">
+      <table>{rows}</table>
+    </div>
+  )
 }
